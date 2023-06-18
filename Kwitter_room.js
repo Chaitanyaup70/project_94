6ef96f7612cd368758c8684d729740bad773c4be
Chaitanyaup70 <106642660+Chaitanyaup70@users.noreply.github.com>
2023-06-18 21:25:44 +0530

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAphTrjg5MYaG0E7KxB8DeynFO9f5EW7Ok",
+  authDomain: "kwiter-6f579.firebaseapp.com",
+  databaseURL: "https://kwiter-6f579-default-rtdb.firebaseio.com",
+  projectId: "kwiter-6f579",
+  storageBucket: "kwiter-6f579.appspot.com",
+  messagingSenderId: "551901858065",
+  appId: "1:551901858065:web:0cf598bb4f9e567fd864b0"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+  
